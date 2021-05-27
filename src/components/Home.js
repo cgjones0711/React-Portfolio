cec0477d3da90075cc64312f8bf271101637fd08
Clay Jones <cgjones0711@gmail.com>
2021-05-26 23:07:46 -0500

    projects added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import bomb from "../images/bombs_away.png"
+import bombs from "../images/bombs_away.png"
 import "../styles/styles.css"
 
 
@@ -8,14 +8,14 @@ export default function Home() {
       <section className="hero is-primary" style={{backgroundColor:"black", }}>
       <div className="hero-body">
       <p className="title">
-     Full-Stack Developer
+     UX/UI
     </p>
     <p class="subtitle">
       
     </p>
       </div>
       <figure className="center-photo hero-img">
-       <img style={{width: 275, height: 275, borderRadius: 400/ 2,}} src={bomb}>
+       <img style={{width: 275, height: 275, borderRadius: 400/ 2,}} src={bombs}>
        </img>
 </figure>
     </section>
