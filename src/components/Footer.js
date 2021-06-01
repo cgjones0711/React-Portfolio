420523bcda4063a8f8b2a48c14e22116100f1dbb
Clay Jones <cgjones0711@gmail.com>
2021-06-01 13:58:38 -0500

    added icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
      <footer className="footer" style={{backgroundColor: "black"}}>
           <div className="content has-text-centered">
             <p>
-              <strong> CLAYTON JONES DESIGNS</strong> <a target="_blank" rel="noreferrer" href="https://www.github.com/cgjones0711">GITHUB</a> ||
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/clayton-jones-8077101b6">LINKEDIN</a> 
-              
+              <strong> CLAYTON JONES DESIGNS</strong> <a target="_blank" rel="noreferrer" href="https://www.github.com/cgjones0711"><i class="fab fa-github"></i></a> ||
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/clayton-jones-8077101b6"><i class="fab fa-linkedin"></i></a> ||
+              <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1PSCP6vmhP6IRwB0qKuIO4SjsZARbt1II/view?usp=sharing"><i class="fas fa-file-pdf"></i>RESUME</a>               
             </p>
           </div>
         </footer>
